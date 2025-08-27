c7be69d12a25992362f0a4dfad00ecc8ec64254b
Enrique Hernández Bello <enrique.hernandez@maarlab.com>
2025-08-27 15:17:33 +0100

    refactor: Simplify Flux deployment with helm_release
    Migrate Flux Operator and Flux Instance deployments from `kubectl_manifest`
    resources, which applied `helm_template` output, to direct `helm_release`
    resources.
    
    This refactoring streamlines the deployment process for Flux CD components
    by leveraging the `helm_release` resource's native capabilities for chart
    management. It removes the need for separate `data.helm_template` and
    `kubectl_manifest` resources, simplifying the Terraform configuration.
    
    Additionally, the explicit `kubernetes_namespace` resource for `flux-system`
    has been removed, as `helm_release` now handles namespace creation
    automatically via the `create_namespace` argument. Dependencies for the
    `flux_secret` resource have also been updated to reflect the new deployment
    flow.

--- manifest_flux.tf
+++ manifest_flux.tf
@@ -1,56 +1,34 @@
-data "helm_template" "flux_operator" {
-  name      = "flux-operator"
-  namespace = "kube-system"
+resource "helm_release" "flux_operator" {
+  depends_on = [helm_release.cilium]
 
-  repository   = "oci://ghcr.io/controlplaneio-fluxcd/charts"
-  chart        = "flux-operator"
-  version      = var.flux_operator_version
-  kube_version = var.kubernetes_version
-
-  values = var.flux_operator_values
+  name             = "flux-operator"
+  namespace        = "flux-system"
+  repository       = "oci://ghcr.io/controlplaneio-fluxcd/charts"
+  chart            = "flux-operator"
+  create_namespace = true
+  version          = var.flux_operator_version
+  values           = var.flux_operator_values
 }
 
-data "kubectl_file_documents" "flux_operator" {
-  content = data.helm_template.flux_operator.manifest
-}
+resource "helm_release" "flux_instance" {
+  count      = var.flux_instance_values == null ? 0 : 1
+  depends_on = [helm_release.flux_operator]
 
-resource "kubectl_manifest" "apply_flux_operator" {
-  for_each   = var.control_plane_count > 0 ? data.kubectl_file_documents.flux_operator.manifests : {}
-  yaml_body  = each.value
-  apply_only = true
-  depends_on = [data.http.talos_health]
-}
-
-data "helm_template" "flux_instance" {
-  name      = "flux-instance"
-  namespace = "kube-system"
-
-  repository   = "oci://ghcr.io/controlplaneio-fluxcd/charts"
-  chart        = "flux-instance"
-  version      = var.flux_instance_version
-  kube_version = var.kubernetes_version
-
-  values = var.flux_instance_values
-}
-
-data "kubectl_file_documents" "flux_instance" {
-  content = data.helm_template.flux_instance.manifest
-}
-
-resource "kubectl_manifest" "apply_flux_instance" {
-  for_each   = var.control_plane_count > 0 ? data.kubectl_file_documents.flux_instance.manifests : {}
-  yaml_body  = each.value
-  apply_only = true
-  depends_on = [data.http.talos_health]
+  name       = "flux-instance"
+  namespace  = "flux-system"
+  repository = "oci://ghcr.io/controlplaneio-fluxcd/charts"
+  chart      = "flux-instance"
+  version    = var.flux_instance_version
+  values     = var.flux_instance_values
 }
 
 resource "kubernetes_secret" "flux_secret" {
   count      = var.flux_secret_username == null || var.flux_secret_password == null ? 0 : 1
-  depends_on = [data.http.talos_health]
+  depends_on = [helm_release.flux_operator]
 
   metadata {
     name      = var.flux_secret_name
-    namespace = "kube-system"
+    namespace = "flux-system"
   }
 
   data = {
