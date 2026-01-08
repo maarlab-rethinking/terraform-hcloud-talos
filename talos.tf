1393c2fc09ba8f39ed830197da16808fee1365be
Enrique Hernández Bello <ehernandez@maarlab.com>
2026-01-08 10:36:41 +0000

    fix(talos): use merged configurations for machine data sources
    Update machine configuration data sources for both control plane and
    worker nodes to use the merged local variables. This ensures that the
    configuration is generated using the processed list.
    
    Signed-off-by: Enrique Hernández Bello <ehernandez@maarlab.com>

--- talos.tf
+++ talos.tf
@@ -73,7 +73,7 @@ locals {
 }
 
 data "talos_machine_configuration" "control_plane" {
-  for_each           = { for control_plane in local.control_planes : control_plane.name => control_plane }
+  for_each           = { for control_plane in local.merged_control_planes : control_plane.name => control_plane }
   talos_version      = var.talos_version
   cluster_name       = var.cluster_name
   cluster_endpoint   = local.cluster_endpoint_url_internal
@@ -86,7 +86,7 @@ data "talos_machine_configuration" "control_plane" {
 }
 
 data "talos_machine_configuration" "worker" {
-  for_each           = { for worker in local.workers : worker.name => worker }
+  for_each           = { for worker in local.merged_workers : worker.name => worker }
   talos_version      = var.talos_version
   cluster_name       = var.cluster_name
   cluster_endpoint   = local.cluster_endpoint_url_internal
