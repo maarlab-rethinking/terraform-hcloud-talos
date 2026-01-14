04bdf080e4cd9ea66f89fb075f21a12ed2d73f19
Enrique Hernández Bello <ehernandez@maarlab.com>
2026-01-14 00:07:56 +0000

    feat(talos): use dynamic version for cloud-controller-manager
    Update the external cloud provider manifest URL to derive the
    version from var.talos_version instead of using a hardcoded string.
    The version is extracted using a regex to maintain compatibility
    across different Talos releases.
    
    Signed-off-by: Enrique Hernández Bello <ehernandez@maarlab.com>

--- talos_patch_control_plane.tf
+++ talos_patch_control_plane.tf
@@ -169,7 +169,7 @@ locals {
         externalCloudProvider = {
           enabled = true
           manifests = [
-            "https://raw.githubusercontent.com/siderolabs/talos-cloud-controller-manager/v1.6.0/docs/deploy/cloud-controller-manager-daemonset.yml"
+            "https://raw.githubusercontent.com/siderolabs/talos-cloud-controller-manager/${regex("^v\\d+\\.\\d+", var.talos_version)}.0/docs/deploy/cloud-controller-manager-daemonset.yml"
           ]
         }
       }
